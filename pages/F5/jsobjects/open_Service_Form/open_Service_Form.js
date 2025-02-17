e5c5583027830de567813af326f667ce2c091af9
Manuel S <manuelsamele@protonmail.com>
2025-02-17 14:44:55 +0000

    F5 modified 1 js object edited open_Service_Form edited

--- pages/F5/jsobjects/open_Service_Form/open_Service_Form.js
+++ pages/F5/jsobjects/open_Service_Form/open_Service_Form.js
@@ -1,12 +1,13 @@
 export default {
 	openServiceFormAdd () {
+		storeValue('configured', 'no')
 		Form3.setVisibility(true)
 		Tabs2.setVisibility(true)
-		storeValue('configured', 'no')
 	},
 	openServiceFormDel () {
-		Tabs2.setVisibility(true)
 		storeValue('configured', 'yes')
+		Tabs2.setVisibility(true)
+		Table2Copy.setVisibility(true)
 	},
 	openPushToDevice () {
 		Button15.setVisibility(true)
