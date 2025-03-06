ab27186b1573812dfd24b9474cdfb9f6340ee54a
Manuel S <manuelsamele@protonmail.com>
2025-03-06 12:17:04 +0000

    Cisco modified 2 queries modified bgpTable edited getTopology added 1 js object edited empty_ParametersBGP edited 1 datasource added Flask - Cisco added

--- pages/Cisco/jsobjects/empty_ParametersBGP/empty_ParametersBGP.js
+++ pages/Cisco/jsobjects/empty_ParametersBGP/empty_ParametersBGP.js
@@ -3,7 +3,8 @@ export default {
     // Get the value of the input fields
     const input1Value = Input1.text;
     const input2Value = Input2.text;
-		const input3Value = Input3.text;
+	  //const input3Value = Input3.text; //hostname
+		const input3Value = MultiSelect5.selectedOptionLabels[0];
 
     // Check if either of the inputs is empty
     if (!input1Value || !input2Value || !input3Value) {
