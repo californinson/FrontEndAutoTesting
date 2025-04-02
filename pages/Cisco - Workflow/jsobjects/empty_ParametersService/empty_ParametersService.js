21b0d6b16f640470cf9735188377eb0fca4182fb
Manuel S <manuelsamele@protonmail.com>
2025-04-02 15:49:01 +0000

    Cisco - Workflow added 3 queries added getTopology added getResoucesWorkflow added UserLogsIn added 8 js objects added open_Service_Form added empty_ParametersCpu added cisco_Empty_Parameters_Check added add_service_to_db added empty_ParametersThroughput added manageModal_f5_cpu added manageModal_f5_conn_sess added empty_ParametersService added Application settings modified

---
+++ pages/Cisco - Workflow/jsobjects/empty_ParametersService/empty_ParametersService.js
@@ -0,0 +1,21 @@
+export default {
+	checkInputsService() {
+    // Get the value of the input fields
+    const input9Value = Input9.text;
+		const input9CopyValue = Input9Copy.text;
+    const input10Value = Input10.text;
+		const input11Value = Input11.text;
+		const input2Value = MultiSelect2.selectedOptionLabels.length;
+		const input3Value = MultiSelect3.selectedOptionLabels.length;
+		const input22Value = MultiSelect2Copy.selectedOptionLabels.length;
+
+    // Check if either of the inputs is empty
+		if(input2Value > 1 || input3Value > 1 || input22Value > 1 || !input9Value || !input10Value || !input11Value || !input9CopyValue){
+			return false;
+		} else {
+			return true;
+		} 
+		
+	}
+
+}
---
+++ pages/Cisco - Workflow/jsobjects/open_Service_Form/open_Service_Form.js
@@ -0,0 +1,21 @@
+export default {
+	openServiceFormAdd () {
+		storeValue('configured', 'no')
+		Tabs2.setVisibility(true)
+		Form3.setVisibility(true)
+		Text27.setVisibility(true)
+		Table2.setVisibility(true)
+		Button15.setVisibility(true)
+	},
+	openServiceFormDel () {
+		storeValue('configured', 'yes')
+		Tabs2.setVisibility(true)
+		Table2Copy.setVisibility(true)
+		Text27Copy.setVisibility(true)
+		Table2Copy.setVisibility(true)
+		Button15Copy.setVisibility(true)
+	},
+	openPushToDevice () {
+		Button15.setVisibility(true)
+	}
+}
